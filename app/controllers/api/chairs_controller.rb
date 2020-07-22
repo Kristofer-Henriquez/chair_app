b5259408f70bbe815bc71ddc76ad4294ffeaa8c0
Kristofer <kristoferhenriquez@gmail.com>
2020-07-21 21:32:15 -0500

    made index action, corrected some paths

--- app/controllers/api/chairs_controller.rb
+++ app/controllers/api/chairs_controller.rb
@@ -1,7 +1,13 @@
 class Api::ChairsController < ApplicationController
 
-def index
-  
-end
+  def index
+    @chairs = Chair.all
+    render "index.html.erb"
+  end
+
+  def show
+    @soda = Soda.find_by(id: params[:id])
+    render "show.html.erb"
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
+
     get "/chairs" => "chairs#index"
+    get "/chairs/:id" => "chairs#show"
   end
 end
